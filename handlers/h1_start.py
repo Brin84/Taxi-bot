73657cd24a4ccf92c13d162e3da144aebb2862e4
Brin84 <brin1984@mail.ru>
2025-08-19 23:02:14 +0300

    Добавил проверку в h1_start и вывел каждому свою клавиатуру

--- handlers/h1_start.py
+++ handlers/h1_start.py
@@ -1,31 +1,34 @@
+# handlers/h1_start.py
 from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
-from keyboards.reply import reply_drive_menu
-from utils.auth import is_driver, is_admin
+from utils.auth import check_access, is_driver, is_admin
+from keyboards.reply import reply_drive_menu, reply_admin_menu  # меню водителя и админа
 
 router = Router()
 
-
 @router.message(F.text == "/start")
 async def h1_start(message: Message):
-    """Приветственное меню и меню водителя/админа"""
+    """Приветственное меню с проверкой прав и выводом своей клавиатуры"""
+    access = await check_access(message)
+    if not access:
+        return
+
     photo = FSInputFile("media/welcome.jpeg")
 
-
     if is_admin(message.from_user.id):
-        text = f"Добро пожаловать, админ! <i>{message.from_user.full_name}</i>"
-
-        await message.answer_photo(photo=photo, caption=text, parse_mode="HTML")
+        await message.answer_photo(
+            photo=photo,
+            caption=f"Добро пожаловать, админ! <i>{message.from_user.full_name}</i>",
+            parse_mode="HTML",
+            reply_markup=reply_admin_menu()
+        )
         return
 
     if is_driver(message.from_user.id):
-        text = f"Добро пожаловать, водитель! <i>{message.from_user.full_name}</i>"
         await message.answer_photo(
             photo=photo,
-            caption=text,
+            caption=f"Добро пожаловать, водитель! <i>{message.from_user.full_name}</i>",
             parse_mode="HTML",
             reply_markup=reply_drive_menu()
         )
         return
-
-    await message.answer("❌ У вас нет доступа к боту.")
