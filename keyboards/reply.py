d916782cc7e7ab6b060091610300089452bfdf20
Brin84 <brin1984@mail.ru>
2025-08-19 23:10:32 +0300

    Создал функцию кнопки income_menu (reply) вызов при реакции на нажатие Доход

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,11 +1,31 @@
+# keyboards/reply.py
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def reply_drive_menu():
-    """Главное меню для водителя"""
-    kb = ReplyKeyboardBuilder()
-    kb.button(text="➕ Добавить доход")
-    kb.button(text="➖ Добавить расход")
-    kb.button(text="📊 Отчёт")
-    kb.adjust(1, 2)
-    return kb.as_markup(resize_keyboard=True)
+    """Клавиатура для водителя"""
+    build = ReplyKeyboardBuilder()
+    build.button(text="Добавить доход 💰")
+    build.button(text="Добавить расход 🧾")
+    build.button(text="Отчёт 📊")
+    build.adjust(1, 3)
+    return build.as_markup(resize_keyboard=True)
 
+def reply_admin_menu():
+    """Клавиатура для администратора"""
+    build = ReplyKeyboardBuilder()
+    build.button(text="Сводка по водителям 📑")
+    build.button(text="Добавить водителя ➕")
+    build.button(text="Удалить водителя ❌")
+    build.button(text="Экспорт данных 📤")
+    build.button(text="Настройки ⚙")
+    build.adjust(1, 3)
+    return build.as_markup(resize_keyboard=True)
+
+def income_menu():
+    """Клавиатура для меню дохода водителя"""
+    build = ReplyKeyboardBuilder()
+    build.button(text="Добавить наличные 💵")
+    build.button(text="Добавить безнал 🏦")
+    build.button(text="Назад 🔙")
+    build.adjust(1, 2)
+    return build.as_markup(resize_keyboard=True)
