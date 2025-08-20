6eb52bfb25348155faa59c80968cea6e5ac83278
Brin84 <brin1984@mail.ru>
2025-08-20 16:40:02 +0300

    подключил обработчики в main

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from handlers import h1_start
-from handlers.drivers import h1_income
+from handlers.drivers import h1_income, h2_expense, h3_report
+from handlers.admin import h1_summary, h2_export, h3_add_driver, h4_remove_driver, h_back_to_admin_menu
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(h1_start.router)
 dp.include_router(h1_income.router)
+dp.include_router(h2_expense.router)
+dp.include_router(h3_report.router)
+dp.include_router(h1_summary.router)
+dp.include_router(h2_export.router)
+dp.include_router(h3_add_driver.router)
+dp.include_router(h4_remove_driver.router)
+dp.include_router(h_back_to_admin_menu.router)
 
 async def main():
     await dp.start_polling(bot)
