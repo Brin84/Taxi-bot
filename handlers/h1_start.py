ef9f0c8ebeadeb726be68f1558db615306f843e4
Brin84 <brin1984@mail.ru>
2025-08-20 12:02:21 +0300

    исправил задачу с доступом прав админа и водителя

--- handlers/h1_start.py
+++ handlers/h1_start.py
@@ -1,34 +1,25 @@
-# handlers/h1_start.py
 from aiogram import Router, F
-from aiogram.types import Message, FSInputFile
-from utils.auth import check_access, is_driver, is_admin
-from keyboards.reply import reply_drive_menu, reply_admin_menu  # меню водителя и админа
+from aiogram.types import Message
+from utils.auth import is_driver, is_admin
+from keyboards.reply import reply_drive_menu, reply_admin_menu
 
 router = Router()
 
 @router.message(F.text == "/start")
 async def h1_start(message: Message):
     """Приветственное меню с проверкой прав и выводом своей клавиатуры"""
-    access = await check_access(message)
-    if not access:
-        return
-
-    photo = FSInputFile("media/welcome.jpeg")
-
-    if is_admin(message.from_user.id):
-        await message.answer_photo(
-            photo=photo,
-            caption=f"Добро пожаловать, админ! <i>{message.from_user.full_name}</i>",
-            parse_mode="HTML",
+    user_id = message.from_user.id
+    if is_admin(user_id):
+        await message.answer(
+            text="Добро пожаловать, админ!",
             reply_markup=reply_admin_menu()
         )
-        return
-
-    if is_driver(message.from_user.id):
-        await message.answer_photo(
-            photo=photo,
-            caption=f"Добро пожаловать, водитель! <i>{message.from_user.full_name}</i>",
-            parse_mode="HTML",
+    elif is_driver(user_id):
+        await message.answer(
+            text="Добро пожаловать, водитель!",
             reply_markup=reply_drive_menu()
         )
-        return
+    else:
+        await message.answer(
+            text="Обратитесь для работы с ботом к администратору."
+        )
--- utils/auth.py
+++ utils/auth.py
@@ -1,18 +1,10 @@
-from config import ADMINS, DRIVERS
-from aiogram.types import Message
+from config import ADMIN, DRIVERS
 
 def is_admin(user_id: int) -> bool:
     """Проверка, является ли пользователь администратором"""
-    return user_id in ADMINS
+    return user_id == ADMIN
 
 def is_driver(user_id: int) -> bool:
     """Проверка, является ли пользователь водителем"""
     return user_id in DRIVERS
 
-async def check_access(message: Message) -> bool:
-    """Проверка доступа пользователя при входе в бот."""
-    user_id = message.from_user.id
-    if is_admin(user_id) or is_driver(user_id):
-        return True
-    await message.answer("❌ У вас нет доступа к боту.")
-    return False
