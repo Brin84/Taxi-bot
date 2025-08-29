7996e30d3e4b6f28f8bbd6b9ca322e82f5d893d3
Brin84 <brin1984@mail.ru>
2025-08-29 16:40:28 +0300

    реализовал модуль для GS с функциями записи в таблицу и получения данных за разные периоды по разным пользователям

--- config.py
+++ config.py
@@ -8,5 +8,4 @@ BOT_TOKEN = os.getenv("TOKEN")
 ADMIN = int(os.getenv("ADMIN"))
 DRIVERS = [6877046695]
 
-GOOGLE_CREDS_FILE = "taxi_bot_google_sheets.json"
-SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+GOOGLE_CREDENTIALS_PATH = os.getenv("GOOGLE_CREDS_PATH", "services/creds/taxi_bot_google_sheets.json")
